5adbae3c5808e5f6af1e2ee16b704c6ad3e93ec9
Goutham <goutham375@gmail.com>
2024-11-24 20:32:25 +0530

    deploy

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use("*", (req, res) => {
 // Server
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running`);
 });
